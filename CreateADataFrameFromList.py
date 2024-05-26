388b645866bbc0eef37cb3b4fd588da2ce828905
LarryTheFatCat <tarkan.zarrouk@gmail.com>
2024-05-26 13:18:13 -0400

    #2877

---
+++ CreateADataFrameFromList.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def createDataframe(student_data: list[list[int]]) -> pd.DataFrame:
+    return pd.DataFrame(student_data,columns=["student_id","age"])
+    
+def main():
+    print(createDataframe([[1,15],[2,11],[3,11],[4,20]]))
+    
+if __name__ == "__main__":
+    main()
