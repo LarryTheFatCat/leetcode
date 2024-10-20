06a2d4618643b3d491c73369527e96ed0ac3af7b
Tarkan Zarrouk <83801755+LarryTheFatCat@users.noreply.github.com>
2024-10-20 01:42:37 -0400

    remove thoughts

--- RankTransformationOfAnArray.py
+++ RankTransformationOfAnArray.py
@@ -3,14 +3,6 @@ def arrayRankTransform(arr:list[int]) -> list[int]:
     :type arr: List[int]
     :rtype: List[int]
     """
-    """
-    1) Two Arrays
-        a) One sorted
-        b) One unsorted
-    2) Assign a temp variable that stores each number from the unsorted array
-    3) Compare that temp variable to each number within the sorted list
-    4) once found, append to new list
-    """
     sorted_list = sorted(arr)
     ranked_array = []
     rank = 1
